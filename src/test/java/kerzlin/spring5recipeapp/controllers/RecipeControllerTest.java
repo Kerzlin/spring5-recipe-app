cca8fd096fdaef8b05c8a1ee6653856baf89c882
kerzlin <natalija.kerzlin@gmail.com>
2021-02-25 18:46:36 +0100

    230. adding number format exception

--- src/test/java/kerzlin/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/kerzlin/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -2,7 +2,6 @@ package kerzlin.spring5recipeapp.controllers;
 
 import kerzlin.spring5recipeapp.commands.RecipeCommand;
 import kerzlin.spring5recipeapp.domain.Recipe;
-import kerzlin.spring5recipeapp.exceptions.NotFoundException;
 import kerzlin.spring5recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,13 +44,14 @@ public class RecipeControllerTest {
                 .andExpect(model().attributeExists("recipe"));
     }
 
+
     @Test
-    public void testGetRecipeNotFound() throws Exception {
-        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
-        mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound())
-                .andExpect(view().name("404error"));
+    public void testGetRecipeNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/asf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
+
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
