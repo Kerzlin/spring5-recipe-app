c2e9a780b15fc572ab4d2165feef0197ba069da2
kerzlin <natalija.kerzlin@gmail.com>
2021-03-02 10:36:58 +0100

    For circleci

--- src/test/java/kerzlin/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/kerzlin/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -49,7 +49,8 @@ public class RecipeControllerTest {
     public void testGetRecipeNumberFormatException() throws Exception {
         mockMvc.perform(get("/recipe/asf/show"))
                 .andExpect(status().isBadRequest())
-                .andExpect(view().name("400error"));
+                .andExpect(view().name("400" +
+                        "error"));
     }
 
 
@@ -72,12 +73,28 @@ public class RecipeControllerTest {
 
         mockMvc.perform(post("/recipe")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("id", "")
-//                .param("description", "some string")
-        )
+                .param("id", "")
+                .param("description", "some string")
+                .param("directions", "some directions"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", ""))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
 
     @Test
     public void testGetUpdateView() throws Exception {
